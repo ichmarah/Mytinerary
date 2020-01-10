ce04186e4bcb29cc6eb2c2f4682c1cba1e51af9b
ichmarah <ichmarah@gmail.com>
2020-01-10 22:36:56 +0100

    Edit an recreate files

--- server/server.js
+++ server/server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log("Server is running on " + port + "port");
+  console.log('Server is running on ' + port + 'port');
 });
 
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 app.use(bodyParser.json());
 app.use(
@@ -18,7 +18,7 @@ app.use(cors());
 
 const db = require('./keys').mongoURI;
 
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true })
     .then(() => console.log('Connection to Mongo DB established'))
